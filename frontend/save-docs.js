0adce0d06f7d55799388c94ca0215fb38cef7a1b
gergo0328 <gergo0328@gmail.com>
2022-06-01 11:35:44 +0200

    little backend

--- frontend/save-docs.js
+++ frontend/save-docs.js
@@ -1,16 +1,14 @@
 setInterval(save, 3000);
 
-function save() {
+async function save() {
     const textarea = document.querySelector("#doc-text");
     const docText = textarea.value;
 
-    const saveText = document.querySelector('#save-in-progress-text');
-    saveText.classList.remove('hidden');
-    fetch('http://127.0.0.1:9000/', {
-        method: 'POST',
+    const saveText = document.querySelector("#save-in-progress-text");
+    saveText.classList.remove("hidden");
+    await fetch("http://127.0.0.1:3000/save", {
+        method: "POST",
         body: docText
-    }).then(function(){
-        saveText.classList.add('hidden');
     });
-
+    saveText.classList.add("hidden");
 }
--- frontend/script.js
+++ frontend/script.js
@@ -1,32 +1,14 @@
-/*const googleSearchButton = document.querySelector("#btn-google-search");
-
-googleSearchButton.addEventListener("click", function(event){
-    event.preventDefault();
-
-    const form = document.querySelector("#form-google-search");
-    const searchTerm = form.querySelector("input[name="q"]").value;
-
-    if(searchTerm ===''){
-        alert('Írj már be valamit!!')
-    }else{
-        form.submit();
-    }
-});*/
-
 const googleSearchButton = document.querySelector("#btn-google-search");
 
-googleSearchButton.addEventListener("click", ButtonClick);
-
-function ButtonClick (event) {
+googleSearchButton.addEventListener("click", function (event) {
     event.preventDefault();
 
     const form = document.querySelector("#form-google-search");
-
     const searchTerm = form.querySelector("input[name='q']").value;
 
     if (searchTerm === "") {
-        alert("Write something.")
+        alert("Írj már be valamit!!");
     } else {
-        form.submit()
+        form.submit();
     }
-};
+});
